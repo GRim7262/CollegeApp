7002f11da255b27775c427bea106202b018673a5
suyog nimje <suyognimje@gmail.com>
2022-08-14 13:11:13 +0530

    College App

--- app/src/main/java/com/example/collegeapp/UploadImage.java
+++ app/src/main/java/com/example/collegeapp/UploadImage.java
@@ -1,14 +1,28 @@
 package com.example.collegeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class UploadImage extends AppCompatActivity {
+
+    private Spinner imageCategory;
+    private CardView selectImage;
+    private Button uploadImage;
+    private ImageView galleryImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_image);
+        selectImage = findViewById(R.id.addGalleryImage);
+        imageCategory = findViewById(R.id.spinner_Of_Image);
+        uploadImage = findViewById(R.id.uploadImageBtn);
+        galleryImageView = findViewById(R.id.galleryImageView);
     }
 }
