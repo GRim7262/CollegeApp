c0cd3297e9bf8de69c7502c422a2bf8d4761c453
suyog nimje <suyognimje@gmail.com>
2022-08-19 12:42:27 +0530

    College App

---
+++ app/src/main/java/com/example/collegeapp/notice/DeleteNoticeActivity.java
@@ -0,0 +1,16 @@
+package com.example.collegeapp.notice;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.collegeapp.R;
+
+public class DeleteNoticeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_notice);
+    }
+}
