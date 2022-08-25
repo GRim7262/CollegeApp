a900b3d6c1d673e49d35963cd32e7118380c1fe6
suyog nimje <suyognimje@gmail.com>
2022-08-25 12:42:02 +0530

    College App

--- app/src/main/java/com/example/collegeapp/notice/NoticeAdapter.java
+++ app/src/main/java/com/example/collegeapp/notice/NoticeAdapter.java
@@ -12,6 +12,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.collegeapp.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
