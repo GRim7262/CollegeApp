e33978bbf2fdae6ddd6eb5f88f2eeffca2e1b53b
GRim7262 <vinalpawar1@gmail.com>
2022-08-26 20:20:16 +0530

    --Updates & Issues fixed-- 1.Added Firebase Analytics New After Bug Fixes. 2.Added Text field in Notice section. 3.Fixed getInstance Error source: stackoverflow (MainActivity.java) has intent issue from line 26. 4.Connected Gallery to firebase. 5.Created EBook Section. 6.Fixed Intent setType for all kinds of documents via stackoverflow. 7.Added Notice Delete. ("application/*")

--- app/src/main/java/com/example/collegeapp/notice/NoticeAdapter.java
+++ app/src/main/java/com/example/collegeapp/notice/NoticeAdapter.java
@@ -7,11 +7,18 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.collegeapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -46,7 +53,20 @@ public class NoticeAdapter extends RecyclerView.Adapter<NoticeAdapter.NoticeView
         holder.deleteNotice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-
+                DatabaseReference reference = FirebaseDatabase.getInstance("https://college-app-admin-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference().child("Notice");
+                reference.child(currentItem.getKey()).removeValue()
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Toast.makeText(context, "Deleted", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(context, "Something went wrong", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                notifyItemRemoved(position);
             }
         });
         
